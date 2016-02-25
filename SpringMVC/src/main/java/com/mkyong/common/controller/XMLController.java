5b374cdd430a71b5e9adc0ece7b6cca4b6f9ca7f
Nischal Samji <nischalsamji@gmail.com>
2016-02-24 17:36:15 -0700

    Push and Pull

--- SpringMVC/src/main/java/com/mkyong/common/controller/XMLController.java
+++ SpringMVC/src/main/java/com/mkyong/common/controller/XMLController.java
@@ -3,10 +3,20 @@ package com.mkyong.common.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.StringWriter;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,38 +36,36 @@ import com.mkyong.common.model.Coffee;
 @Controller
 public class XMLController {
 
-	@RequestMapping(value="/",headers="Accept=*/*", method = RequestMethod.GET)
-	public String getCoffeeInXML() {
-
-		return "hello";
-	    
-
-	}
-	
-	@RequestMapping(value="/greeting",headers="Accept=*/*", method=RequestMethod.POST)
+	@RequestMapping(value="/", headers="Accept=*/*", method=RequestMethod.GET)
     public String greetingSubmit( Model model) {
         
         return "no hello";
     }
 	
 	
-	@RequestMapping(value="/upload",headers="Accept=*/*", method=RequestMethod.POST)
-    public @ResponseBody Document handleFileUpload(@RequestParam("name") String name,
-            @RequestParam("file") MultipartFile file){
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+    public String  handleFileUpload(HttpServletResponse response, @RequestParam("file") MultipartFile file){
         if (!file.isEmpty()) {
         try{
+            response.setContentType("application/xml");
             File inputFile = new File(file.getOriginalFilename());
             file.transferTo(inputFile);
+         
             DocumentBuilderFactory dbFactory 
                = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
-            
+            System.out.println(doc.getDocumentURI());
             doc.getDocumentElement().normalize();
             System.out.println(doc.toString());
             System.out.println("Root element :" 
                + doc.getDocumentElement().getNodeName());
-            
+            OutputFormat format = new OutputFormat(doc);
+            String filename = "mydocument"+System.currentTimeMillis()+".xml";
+            @SuppressWarnings("deprecation")
+            XMLSerializer serializer = new XMLSerializer(
+                new FileOutputStream(new File(filename)),format);
+            serializer.serialize(doc);
             NodeList nList = doc.getChildNodes();
             System.out.println("----------------------------");
             for (int temp = 0; temp < nList.getLength(); temp++) {
@@ -69,8 +77,16 @@ public class XMLController {
  
                }
             }
-            System.out.println(doc);
-            return doc;
+            TransformerFactory tf = TransformerFactory.newInstance();
+            Transformer transformer = tf.newTransformer();
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StringWriter writer = new StringWriter();
+            transformer.transform(new DOMSource(doc), new StreamResult(writer));
+            String output = writer.getBuffer().toString().replaceAll("\n|\r", "");
+            System.out.println(output);
+            
+            response.setContentType("text/xml");
+            return output; 
             
          } catch (Exception e) {
         	 
--- SpringMVC/src/main/webapp/WEB-INF/no hello.jsp
+++ SpringMVC/src/main/webapp/WEB-INF/no hello.jsp
@@ -1,18 +1,84 @@
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-<html>
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en">
 <head>
-    <title>Spring MVC Form Handling</title>
+<meta charset="utf-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Sample XXE Attack</title>
+
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/normalize.min.css">
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/css/foundation.min.css">
+<link
+	href='http://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css'
+	rel='stylesheet' type='text/css'>
+<style>
+.file-upload {
+  position: relative;
+  overflow: hidden;
+  margin: 10px; }
+
+.file-upload input.file-input {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 0;
+  padding: 0;
+  font-size: 20px;
+  cursor: pointer;
+  opacity: 0;
+  filter: alpha(opacity=0); }
+</style>
+
+<script
+	src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
 <body>
+	<div class="row" align ="center">
+		<div class="small-12 columns">
+			<ul class="button-group">
+				<li><a href="" class="button">Source Code</a></li>
+				<li><a href="" class="button">Virtual Machine</a></li>
+			</ul>
+		</div>
+	</div>
 
-<h2>Student Information</h2>
 
-    <form:form method="POST" enctype="multipart/form-data" action="upload">
-		File to upload: <input type="file" name="file"><br /> Name: <input
-			type="text" name="name"><br /> <br /> <input type="submit"
-			value="Upload"> Press here to upload the file!
-	</form:form>
-  
+	<div class="row">
+		<h2 align="center">Sample XXE Attack</h2>
+		<form:form method="POST" enctype="multipart/form-data" action="upload">
+			<table align="center">
 
+				<tr>
+					<td><h4>File to upload:</h4></td>
+					<td><h4><input class="file-upload" type="file" name="file"></h4></td>
+				</tr>
+				<tr>
+					<td><input class="radius button right" type="submit" value="Upload"></td>
+					<td><h4>Press here to upload the file!</h4></td>
+				</tr>
+
+			</table>
+		</form:form>
+	</div>
+
+	<footer class="row">
+		<div class="large-12 columns">
+			<hr />
+			<div class="row">
+				<div class="large-6 columns">
+					<p>&copy; Done as part of ACNS Homework</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/foundation/5.5.3/js/foundation.min.js"></script>
+	<script>
+		$(document).foundation();
+	</script>
 </body>
-</html>
